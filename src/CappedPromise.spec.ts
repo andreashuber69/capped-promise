ca688c50ddfa035bf82ea055d3a30fab0e5bf8b9
Andreas Huber <andreashuber69@users.noreply.github.com>
2023-01-12 14:31:32 +0100

    test: awaitable states

--- src/CappedPromise.spec.ts
+++ src/CappedPromise.spec.ts
@@ -11,6 +11,8 @@ const iterable = function *iterable() {
     yield async () => await Promise.resolve(2);
 };
 
+type State = "created" | "init" | "pending" | "resolved";
+
 describe("CappedPromise", () => {
     describe("constructor", () => {
         it("should throw for invalid options", () => {
@@ -50,6 +52,34 @@ describe("CappedPromise", () => {
 
             expect(result0).to.equal(1);
             expect(result1).to.equal(2);
+        });
+
+        it("should only create new awaitables when all previous ones have been resolved", async () => {
+            const sut = new CappedPromise({ cap: 1 });
+            const states = new Array<State>(5).fill("init");
+
+            const argument = states.map((_, index, array) => (
+                async () => {
+                    // Previous awaitables must be resolved
+                    expect(array.findIndex((state) => state !== "resolved")).to.equal(index);
+                    // Next awaitables must be in initial state
+                    expect(array.slice(index).findIndex((state) => state !== "init")).to.equal(-1);
+                    array[index] = "created";
+                    const promise = Promise.resolve(index);
+                    array[index] = "pending";
+                    const result = await promise;
+                    // eslint-disable-next-line require-atomic-updates
+                    array[index] = "resolved";
+
+                    return result;
+                }
+            ));
+
+            const results = await sut.all(argument);
+
+            for (const [index, value] of results.entries()) {
+                expect(value).to.equal(index);
+            }
         });
 
         it("should throw for non-functions", async () => {
