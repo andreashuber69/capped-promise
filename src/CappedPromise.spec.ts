c14e34f2165c1b63aa01e6070b3fcd97f18bd8ec
Andreas Huber <andreashuber69@users.noreply.github.com>
2023-01-19 18:23:25 +0100

    feat: maxPending is now a parameter of all

--- src/CappedPromise.spec.ts
+++ src/CappedPromise.spec.ts
@@ -14,48 +14,27 @@ const iterable = function *iterable() {
 type State = "init" | "pending" | "settled";
 
 describe("CappedPromise", () => {
-    describe("constructor", () => {
-        it("should throw for invalid options", () => {
-            expect(() => new CappedPromise({ cap: 0 })).to.throw(
-                RangeError,
-                "options.cap is invalid: 0.",
-            );
-        });
-    });
-    describe("cap", () => {
-        it("should be equal to the default", () => {
-            expect(new CappedPromise().cap).to.equal(5);
-        });
-        it("should be equal to options.cap", () => {
-            expect(new CappedPromise({ cap: 42 }).cap).to.equal(42);
-        });
-    });
     describe("all", () => {
         it("should correctly infer the types for tuples", async () => {
-            const sut = new CappedPromise();
-
             const argument = [
                 async () => await Promise.resolve(1),
                 async () => await Promise.resolve("hello"),
             ] as const;
 
-            const [result0, result1]: [number, string] = await sut.all(argument);
+            const [result0, result1]: [number, string] = await CappedPromise.all(5, argument);
 
             expect(result0).to.equal(1);
             expect(result1).to.equal("hello");
         });
 
         it("should correctly infer the types for iterables", async () => {
-            const sut = new CappedPromise();
-
-            const [result0, result1]: number[] = await sut.all(iterable());
+            const [result0, result1]: number[] = await CappedPromise.all(5, iterable());
 
             expect(result0).to.equal(1);
             expect(result1).to.equal(2);
         });
 
         it("should only create new awaitables when all previous ones are pending or settled", async () => {
-            const sut = new CappedPromise({ cap: 3 });
             const states = new Array<State>(5).fill("init");
 
             const argument = states.map((_, index, array) => (
@@ -76,17 +55,22 @@ describe("CappedPromise", () => {
                 }
             ));
 
-            const results = await sut.all(argument);
+            const results = await CappedPromise.all(3, argument);
 
             for (const [index, value] of results.entries()) {
                 expect(value).to.equal(index);
             }
         });
 
-        it("should throw for non-functions", async () => {
-            const sut = new CappedPromise();
+        it("should throw for invalid maxPending", async () => {
+            await expect(CappedPromise.all(0, [])).to.eventually.be.rejectedWith(
+                RangeError,
+                "maxPending is invalid: 0.",
+            );
+        });
 
-            await expect(sut.all([42 as unknown as () => Promise<number>])).to.eventually.be.rejectedWith(
+        it("should throw for non-functions", async () => {
+            await expect(CappedPromise.all(5, [42 as unknown as () => Promise<number>])).to.eventually.be.rejectedWith(
                 TypeError,
                 "createAwaitable is not a function: 42.",
             );
--- src/CappedPromise.ts
+++ src/CappedPromise.ts
@@ -1,56 +1,88 @@
 // https://github.com/andreashuber69/capped-promise#--
-interface Options {
-    readonly cap: number;
-}
-
 export = class CappedPromise {
-    public constructor(options?: Options) {
-        ({ cap: this.#cap } = { cap: 5, ...options });
-
-        if ((typeof this.#cap !== "number") || (this.#cap < 1)) {
-            throw new RangeError(`options.cap is invalid: ${this.#cap}.`);
-        }
-    }
-
-    public get cap() {
-        return this.#cap;
-    }
-
-    public async all<T extends ReadonlyArray<() => unknown>>(
-        createAwaitableArray: T
+    public static async all<T extends ReadonlyArray<() => unknown>>(
+        maxPending: number,
+        createAwaitableArray: T,
     ): Promise<{ -readonly [P in keyof T]: Awaited<ReturnType<T[P]>> }>;
-    public async all<T>(createAwaitableIterable: Iterable<() => PromiseLike<T> | T>): Promise<Array<Awaited<T>>>;
-    public async all(createAwaitableIterable: Iterable<() => unknown>) {
-        const results = new Array<unknown>();
-        const pending = new Array<Promise<number>>();
+    public static async all<T>(
+        maxPending: number,
+        createAwaitableIterable: Iterable<() => PromiseLike<T> | T>,
+    ): Promise<Array<Awaited<T>>>;
+    public static async all(
+        maxPending: number,
+        createAwaitableIterable: Iterable<() => unknown>,
+    ) {
+        const results = await this.#all(maxPending, true, createAwaitableIterable);
+
+        return results.map((r) => (r.status === "fulfilled" ? r.value : undefined));
+    }
+
+    private constructor() {}
+
+    static async #all(
+        maxPending: number,
+        propagateRejections: boolean,
+        createAwaitableIterable: Iterable<() => unknown>,
+    ) {
+        if (typeof maxPending !== "number" || maxPending < 1) {
+            throw new RangeError(`maxPending is invalid: ${maxPending}.`);
+        }
+
+        let nextIndex = 0;
+        const pending = new Map<number, Promise<number>>();
+        const results = new Array<PromiseSettledResult<unknown>>();
 
         for (const createAwaitable of createAwaitableIterable) {
-            pending.push(
-                CappedPromise.#awaitAndStoreResult(createAwaitable, results, pending.length),
-            );
-
-            if (pending.length === this.cap) {
-                // eslint-disable-next-line no-await-in-loop
-                pending.splice(await Promise.race(pending), 1);
+            if (typeof createAwaitable !== "function") {
+                throw new TypeError(`createAwaitable is not a function: ${createAwaitable}.`);
             }
+
+            if (pending.size === maxPending) {
+                // eslint-disable-next-line no-await-in-loop
+                await this.#settleNext(pending, results, propagateRejections);
+            }
+
+            pending.set(nextIndex, this.#storeResult(createAwaitable, results, nextIndex));
+            ++nextIndex;
         }
 
-        await Promise.all(pending);
+        while (pending.size > 0) {
+            // eslint-disable-next-line no-await-in-loop
+            await this.#settleNext(pending, results, propagateRejections);
+        }
 
         return results;
     }
 
-    static async #awaitAndStoreResult(createAwaitable: unknown, results: unknown[], result: number) {
-        if (typeof createAwaitable !== "function") {
-            throw new TypeError(`createAwaitable is not a function: ${createAwaitable}.`);
+    static async #settleNext(
+        pending: Map<number, Promise<number>>,
+        results: Array<PromiseSettledResult<unknown>>,
+        propagateRejections: boolean,
+    ) {
+        const settledIndex = await Promise.race(pending.values());
+        pending.delete(settledIndex);
+        const result = results[settledIndex];
+
+        if (propagateRejections && (result?.status === "rejected")) {
+            throw result.reason;
         }
-
-        const index = results.push(undefined) - 1;
-        // eslint-disable-next-line require-atomic-updates
-        results[index] = await createAwaitable();
-
-        return result;
     }
 
-    readonly #cap: number;
+    static async #storeResult(
+        createAwaitable: () => unknown,
+        results: Array<PromiseSettledResult<unknown>>,
+        index: number,
+    ) {
+        results[index] = await this.#getResult(createAwaitable);
+
+        return index;
+    }
+
+    static async #getResult(createAwaitable: () => unknown): Promise<PromiseSettledResult<unknown>> {
+        try {
+            return { status: "fulfilled", value: await createAwaitable() };
+        } catch (error: unknown) {
+            return { status: "rejected", reason: error };
+        }
+    }
 };
